ab58199e2527826f3582c88cffa22c643ffdeb26
Alex Khomenko <alexkhomenko@cureatr.com>
2024-10-02 00:08:21 +0800

    fix iOS 13 deprecations

--- MBProgressHUD.m
+++ MBProgressHUD.m
@@ -379,7 +379,7 @@ static const CGFloat MBDefaultDetailsLabelFontSize = 12.f;
                 activityIndicator.color = [UIColor whiteColor];
 #if !TARGET_OS_MACCATALYST
             } else {
-               activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+                activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleLarge];
             }
 #endif
             [activityIndicator startAnimating];
@@ -743,14 +743,14 @@ static const CGFloat MBDefaultDetailsLabelFontSize = 12.f;
     NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
 
     [nc addObserver:self selector:@selector(statusBarOrientationDidChange:)
-               name:UIApplicationDidChangeStatusBarOrientationNotification object:nil];
+               name:UIDeviceOrientationDidChangeNotification object:nil];
 #endif
 }
 
 - (void)unregisterFromNotifications {
 #if !TARGET_OS_TV && !TARGET_OS_MACCATALYST
     NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
-    [nc removeObserver:self name:UIApplicationDidChangeStatusBarOrientationNotification object:nil];
+    [nc removeObserver:self name:UIDeviceOrientationDidChangeNotification object:nil];
 #endif
 }
 
